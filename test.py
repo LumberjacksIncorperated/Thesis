b0814b2a71858eaeda5fb55f8a75c6ade5ce5613
LumberjacksIncorperated <35323296+LumberjacksIncorperated@users.noreply.github.com>
2018-10-02 15:19:36 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
-from PIL import Image
-img = Image.open('document.jpeg').convert('L')
-img.save('greyscale.jpeg')
+import Image
+import numpy
+
+def image2pixelarray(filepath):
+        """
+        Parameters
+        ----------
+        filepath : str
+            Path to an image file
+
+        Returns
+        -------
+        list
+            A list of lists which make it simple to access the greyscale value by
+            im[y][x]
+        """
+    im = Image.open(filepath).convert('L')
+    (width, height) = im.size
+    greyscale_map = list(im.getdata())
+    greyscale_map = numpy.array(greyscale_map)
+    greyscale_map = greyscale_map.reshape((height, width))
+    return greyscale_map
+
+image = image2pixelarray("document.jpeg")
+print image
+ 
